445df59b88bcfa540698fbe042f7bf45c61b9429
Navin Moorthy <navin007.a@gmail.com>
2022-12-27 14:53:16 +0530

    build(vite): 🏗️ update vite config to support top level await

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 // vite.config.js
 import { resolve } from "path";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig(() => {
   return {
-    base: "./",
-    publicDir: resolve(__dirname, "website/public"),
     root: resolve(__dirname, "website"),
     build: {
+      target: browserslistToEsbuild(),
       outDir: resolve(__dirname, "dist"),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "/index.html"),
+          maps: resolve(__dirname, "/maps.html"),
+        },
+      },
     },
+    plugins: [topLevelAwait()],
   };
 });
